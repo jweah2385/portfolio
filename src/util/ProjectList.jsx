e18dce34ed69d22fb1011ed318120e777e73d4d4
James Weah <jweah@aggies.ncat.edu>
2023-12-02 21:13:00 -0500

    style: project page rough completed

--- src/util/ProjectList.jsx
+++ src/util/ProjectList.jsx
@@ -7,51 +7,58 @@ import helloSowftware from '../images/helloSoftware.jpg';
 
 const AllProjects = [
   {
-    name: 'Backend Api',
-    ImageUrl: backEndapi,
-    description:
-    'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups',
-    gitHubUrl: 'https://github.com/jweah2385/nosql-network-api',
-  },
-  {
-    name: 'Book Search',
-    ImageUrl: bookSearch,
-    description:
-      'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups',
-    gitHubUrl: 'https://github.com/jweah2385/book-search-api1',
-    deploymentUrl: 'https://book-search-api2-f92e504c17b9.herokuapp.com/',
-  },
-  {
+    id: '1',
     name: 'Ecommerce Front-End',
     ImageUrl: ecommerceFrontEnd,
     description:
-      'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups',
+      'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing ',
     gitHubUrl: 'https://github.com/jweah2385/React-ecommerce',
     deploymentUrl: 'https://react-ecommerce-sigma-rosy.vercel.app/',
   },
   {
+    id: '2',
+    name: 'Book Search',
+    ImageUrl: bookSearch,
+    description:
+      'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing',
+    gitHubUrl: 'https://github.com/jweah2385/book-search-api1',
+    deploymentUrl: 'https://book-search-api2-f92e504c17b9.herokuapp.com/',
+  },
+
+  {
+    id: '3',
     name: 'JavaScript Quiz',
     ImageUrl: jsQuiz,
     description:
-      'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups',
+      'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing',
     gitHubUrl: 'https://github.com/jweah2385/code-Quiz',
     deploymentUrl: 'https://jweah2385.github.io/code-Quiz/',
   },
   {
+    id: '4',
     name: 'Weather App',
     ImageUrl: weatherApp,
     description:
-      'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups',
+      'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing',
     gitHubUrl: 'https://github.com/jweah2385/weather-app',
     deploymentUrl: 'https://jweah2385.github.io/weather-app/',
   },
   {
+    id: '5',
     name: 'Hello Software',
     ImageUrl: helloSowftware,
     description:
-      'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups',
+      'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing',
     gitHubUrl: 'https://github.com/Hans-Doderlein/Hello-Software',
     deploymentUrl: 'https://hello-software-37812ef8ef64.herokuapp.com/',
+  },
+  {
+    id: '6',
+    name: 'Backend Api',
+    ImageUrl: backEndapi,
+    description:
+      'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing',
+    gitHubUrl: 'https://github.com/jweah2385/nosql-network-api',
   },
 ];
 
